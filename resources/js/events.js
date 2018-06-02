c462cc2d9c982bb4b39d2bf94d9f1fc9014f8d6c
jawadsaad <31541931+jawadsaad@users.noreply.github.com>
2018-06-02 19:45:23 +0100

    Add files via upload

---
+++ resources/js/events.js
@@ -0,0 +1,32 @@
+var slideIndex = [1,1,1,1,1];
+var slideId = ["sidal-sidra-slides", "taiba-slides", "yamama-events-slides", "shebstan-events-slides", "garden-events-slides"];
+showSlides(1,0);
+showSlides(1,1);
+showSlides(1,2);
+showSlides(1,3);
+showSlides(1,4);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+// Thumbnail image controls
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var slides = document.getElementsByClassName(slideId[no]);
+//  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex[no] = 1} 
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+//  for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" active", "");
+//  }
+  slides[slideIndex[no]-1].style.display = "block"; 
+//  dots[slideIndex-1].className += " active";
+}
